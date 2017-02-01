abedcdfdf0d48acc6e572d227e6f794ff58f4b28
Nick <proulx_nick06@hotmail.com>
2017-01-31 19:42:58 -0500

    - removed dummy data output - created a Node class and made a processing method to dynamically create a tree structure given a root node - the given old (root) node now has children appended to it as well as a state

--- bridge_crossing/bridge_crossing.py
+++ bridge_crossing/bridge_crossing.py
@@ -1,14 +1,22 @@
+class BridgeCrossException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
+
 class Runner:
+
     def __init__(self, run_time):
         self.run_time = run_time
 
 
 class State:
-    def __init__(self, left_num, torch, right_num):
+
+    def __init__(self, left_num, torch, right_num, run_time):
         self.runners_left = left_num
         self.torch = torch
         self.runners_right = right_num
-        self.parent = None
+        self.run_time = run_time
 
     def is_at_goal(self):
         if self.runners_left == 0 \
@@ -25,54 +33,13 @@ class State:
             return False
 
 
-def process_crossing(state):
-    # child_nodes = list()
-    if state.is_at_goal():
-        return 'reached goal'
+class Node:
 
-    elif state.torch == 'Left':  # the torch is on the left -- must cross right
-        # moving two runners to the right
-        new_state = State(state.runners_left - 2, 'Right', state.runners_right + 2)
-        if new_state.is_valid_cross():
-            new_state.parent = state
-            # child_nodes.append(new_state)
-            return new_state
-
-        # # move one runner from left to the right
-        # new_state = State(state.runners_left - 1, 'Right', state.runners_right + 1)
-        # if new_state.is_valid_cross():
-        #     new_state.parent = state
-        #     child_nodes.append(new_state)
-
-    else:  # the torch is on the right -- must cross left
-        # have one person bring the torch back to the left
-        new_state = State(state.runners_left + 1, 'Left', state.runners_right - 1)
-        if new_state.is_valid_cross():
-            new_state.parent = state
-            # child_nodes.append(new_state)
-            return new_state
-
-
-def run():
-    initial_state = State(6, 'Left', 0)
-    state = process_crossing(initial_state)
-    path = list()
-    path.append(state)
-    while not state.is_at_goal():
-        if state.is_at_goal():
-            return 'Done!!'
-        else:
-            state = process_crossing(state)
-            path.append(state)
-
-    return path
-
-
-def print_path(path):
-    for nodes in path:
-        print 'left: ' + str(nodes.runners_left)
-        print 'torch: ' + nodes.torch
-        print 'right: ' + str(nodes.runners_right) + '\n\n'
+    def __init__(self):
+        self.state = None
+        self.parent = None
+        self.depth = 0
+        self.children = []  # to be used to create possible chile nodes dynamically and append to list
 
 
 # only hard coded for testing
@@ -100,14 +67,76 @@ def initiate_crossing():
     return left_side, torch, right_side
 
 
-def main():
-    # left_runners, torch, right_runners = initiate_crossing()
-    # left_len = len(left_runners)
-    # right_len = len(right_runners)
-    # state = State(left_len, 'Left', right_len)
-    # print process_crossing(state)
+def process_crossing(state, old_node, left_runners, right_runners):
+    frontier = []
 
-    print_path(run())
+    if state.is_at_goal():
+        return 'Already at goal!'
+
+    elif state.torch == 'Left':
+        if len(left_runners) > 1:
+            while len(left_runners) > 1:
+                # move 2 runners at a time across the bridge
+                new_state = State(state.runners_left - 2, 'Right', state.runners_right + 2,
+                                  max(left_runners[0].run_time, left_runners[1].run_time))
+                if new_state.is_valid_cross():
+                    new_node = Node()
+                    new_node.state = new_state
+                    new_node.parent = old_node
+                    new_node.depth = old_node.depth + 1
+                    old_node.children.append(new_node)
+                    frontier.append(new_node)
+                    right_runners.append(left_runners[1])
+                    right_runners.append(left_runners[0])
+                    left_runners.remove(left_runners[1])
+                    left_runners.remove(left_runners[0])
+
+        if len(left_runners) == 1:
+            # maybe for some reason there is less than 2 people and need to bring only one to right
+            new_state = State(state.runners_left - 1, 'Right', state.runners_right + 1, runner.run_time)
+            if new_state.is_valid_cross():
+                new_node = Node
+                new_node.state = new_state
+                new_node.parent = old_node
+                new_node.depth = old_node.depth + 1
+                old_node.children.append(new_node)
+                frontier.append(new_node)
+
+        return frontier, left_runners, right_runners
+
+    elif state.torch == 'Right':
+        for runner in right_runners:
+            new_state = State(state.runners_left + 1, 'Left', state.runners_right - 1, runner.run_time)
+            if new_state.is_valid_cross():
+                new_node = Node
+                new_node.state = new_state
+                new_node.parent = old_node
+                new_node.depth = old_node.depth + 1
+                old_node.children.append(new_node)
+                frontier.append(new_node)
+
+        return frontier, left_runners, right_runners
+
+    else:
+        raise BridgeCrossException('Invalid torch placement')
+
+
+def breadth_first():
+    # initialize the root state and create a new root node with that state
+    left_side, torch, right_side = initiate_crossing()
+    left_runners = len(left_side)
+    right_runners = len(right_side)
+    initial_state = State(left_runners, torch, right_runners, 0)
+    root_node = Node()
+    root_node.depth = 0
+    root_node.parent = None
+    root_node.state = initial_state
+
+    fronteir, left_runners, right_runners = process_crossing(initial_state, root_node, left_side, right_side)
+
+
+def main():
+    breadth_first()
 
 
 if __name__ == '__main__':
