c3e6f471032be4d36f932134b01cf7ab63aebd46
Nick <proulx_nick06@hotmail.com>
2017-02-01 20:52:29 -0500

    added a breadth-first search, process is finishing and printing the most efficient path.

--- bridge_crossing/bridge_crossing.py
+++ bridge_crossing/bridge_crossing.py
@@ -20,7 +20,8 @@ class State:
 
     def is_at_goal(self):
         if self.runners_left == 0 \
-           and self.torch == 'Right':
+           and self.torch == 'Right' \
+           and self.runners_right > 0:
             return True
         else:
             return False
@@ -39,11 +40,11 @@ class Node:
         self.state = None
         self.parent = None
         self.depth = 0
-        self.children = []  # to be used to create possible chile nodes dynamically and append to list
+        self.children = list()  # to be used to create possible chile nodes dynamically and append to list
 
 
 # only hard coded for testing
-def initiate_crossing():
+def initiate_runners():
 
     left_side = list()
     right_side = list()
@@ -67,7 +68,20 @@ def initiate_crossing():
     return left_side, torch, right_side
 
 
-def process_crossing(state, old_node, left_runners, right_runners):
+def path_exists(path_list, new_node):
+    is_old_state = False
+
+    for node in path_list:
+        if new_node.state.runners_left == node.state.runners_left \
+           and new_node.state.runners_right == node.state.runners_right \
+           and new_node.state.torch == node.state.torch:
+            is_old_state = True
+            break
+
+    return is_old_state
+
+
+def successor_processing(state, old_node, left_runners, right_runners):
     frontier = []
 
     if state.is_at_goal():
@@ -93,7 +107,7 @@ def process_crossing(state, old_node, left_runners, right_runners):
 
         if len(left_runners) == 1:
             # maybe for some reason there is less than 2 people and need to bring only one to right
-            new_state = State(state.runners_left - 1, 'Right', state.runners_right + 1, runner.run_time)
+            new_state = State(state.runners_left - 1, 'Right', state.runners_right + 1, left_runners[0].run_time)
             if new_state.is_valid_cross():
                 new_node = Node
                 new_node.state = new_state
@@ -101,21 +115,25 @@ def process_crossing(state, old_node, left_runners, right_runners):
                 new_node.depth = old_node.depth + 1
                 old_node.children.append(new_node)
                 frontier.append(new_node)
+                right_runners.append(left_runners[0])
+                left_runners.remove(left_runners[0])
 
-        return frontier, left_runners, right_runners
+        return old_node, left_runners, right_runners
 
     elif state.torch == 'Right':
         for runner in right_runners:
             new_state = State(state.runners_left + 1, 'Left', state.runners_right - 1, runner.run_time)
             if new_state.is_valid_cross():
-                new_node = Node
+                new_node = Node()
                 new_node.state = new_state
                 new_node.parent = old_node
                 new_node.depth = old_node.depth + 1
                 old_node.children.append(new_node)
                 frontier.append(new_node)
+                left_runners.append(right_runners[0])
+                right_runners.remove(right_runners[0])
 
-        return frontier, left_runners, right_runners
+        return old_node, left_runners, right_runners
 
     else:
         raise BridgeCrossException('Invalid torch placement')
@@ -123,20 +141,54 @@ def process_crossing(state, old_node, left_runners, right_runners):
 
 def breadth_first():
     # initialize the root state and create a new root node with that state
-    left_side, torch, right_side = initiate_crossing()
-    left_runners = len(left_side)
-    right_runners = len(right_side)
-    initial_state = State(left_runners, torch, right_runners, 0)
-    root_node = Node()
-    root_node.depth = 0
-    root_node.parent = None
-    root_node.state = initial_state
+    left_side, torch, right_side = initiate_runners()
+    left_num = len(left_side)
+    right_num = len(right_side)
+    initial_state = State(left_num, torch, right_num, 0)
+    node = Node()
+    node.depth = 0
+    node.parent = None
+    node.state = initial_state
+    run_time = node.state.run_time
 
-    fronteir, left_runners, right_runners = process_crossing(initial_state, root_node, left_side, right_side)
+    path_to_goal = list()
+    path_to_goal.append(node)
+
+    while not node.state.is_at_goal():
+        node, left_side, right_side = successor_processing(node.state,
+                                                           node,
+                                                           left_side,
+                                                           right_side)
+        cheapest_run_time = 0
+        cheapest_node = Node()
+        for each_node in node.children:
+            if not path_exists(path_to_goal, each_node):
+                if cheapest_run_time == 0 \
+                   or each_node.state.run_time < cheapest_run_time:
+                    cheapest_run_time = each_node.state.run_time
+                    cheapest_node = each_node
+        path_to_goal.append(cheapest_node)
+        run_time += cheapest_node.state.run_time
+        node = cheapest_node
+
+    return path_to_goal, run_time
+
+
+def print_path():
+    path, run_time = breadth_first()
+    i = 0
+    for node in path:
+        print 'Move: ' + str(i)
+        print str(node.state.runners_left) + ',  ' + node.state.torch + ',  ' + str(node.state.runners_right)
+        print 'Crossing Time: ' + str(node.state.run_time) + '\n'
+        i += 1
+
+    print 'Done!'
+    print 'Total Time Elapsed: ' + str(run_time)
 
 
 def main():
-    breadth_first()
+    print_path()
 
 
 if __name__ == '__main__':
