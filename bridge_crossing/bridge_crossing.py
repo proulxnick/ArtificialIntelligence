174d03849e4c5545ebf6f54a3ec8f5fd8f2b3fb8
Nick <proulx_nick06@hotmail.com>
2017-02-03 21:52:37 -0500

    - added interactive textual interface - enhanced readability - removed redundant code

--- bridge_crossing/bridge_crossing.py
+++ bridge_crossing/bridge_crossing.py
@@ -1,6 +1,7 @@
 class BridgeCrossException(Exception):
 
     def __init__(self, message):
+        # use as class exception for situational exception handling
         self.message = message
 
 
@@ -13,22 +14,17 @@ class Runner:
 class State:
 
     def __init__(self, left_num, torch, right_num, run_time):
+        # each Node will have a state of this format
         self.runners_left = left_num
         self.torch = torch
         self.runners_right = right_num
         self.run_time = run_time
 
     def is_at_goal(self):
+        # check if the state is equal to the goal
         if self.runners_left == 0 \
            and self.torch == 'Right' \
            and self.runners_right > 0:
-            return True
-        else:
-            return False
-
-    def is_valid_cross(self):
-        if 0 <= self.runners_left <= 6 \
-           and 0 <= self.runners_right <= 6:
             return True
         else:
             return False
@@ -40,30 +36,24 @@ class Node:
         self.state = None
         self.parent = None
         self.depth = 0
-        self.children = list()  # to be used to create possible chile nodes dynamically and append to list
+        self.children = list()  # to be used to create possible child nodes dynamically and append to list
 
 
 # only hard coded for testing
 def initiate_runners():
-
     left_side = list()
     right_side = list()
     torch = 'Left'
 
-    # define the runners
-    r1 = Runner(4)
-    r2 = Runner(6)
-    r3 = Runner(8)
-    r4 = Runner(10)
-    r5 = Runner(12)
-    r6 = Runner(14)
+    total_runners = input('Enter number of runners: ')
+    i = 1  # for control in while loop
 
-    left_side.append(r1)
-    left_side.append(r2)
-    left_side.append(r3)
-    left_side.append(r4)
-    left_side.append(r5)
-    left_side.append(r6)
+    # get the run times of all the runners the user wants to create
+    while i <= total_runners:
+        new_runner_time = input('Enter run time of runner number ' + str(i) + ': ')
+        runner = Runner(new_runner_time)
+        left_side.append(runner)
+        i += 1
 
     # initialize the root state and create a new root node with that state
     left_num = len(left_side)
@@ -75,10 +65,11 @@ def initiate_runners():
     node.state = initial_state
     run_time = node.state.run_time
 
-    return left_side, torch, right_side, node, run_time
+    return left_side, torch, right_side, node, run_time, total_runners
 
 
 def path_exists(path_list, new_node):
+    # this is a method to check if the state of the given node exists in the given path
     is_old_state = False
 
     for node in path_list:
@@ -92,8 +83,6 @@ def path_exists(path_list, new_node):
 
 
 def successor_processing(state, old_node, left_runners, right_runners):
-    frontier = []
-
     if state.is_at_goal():
         return 'Already at goal!'
 
@@ -103,45 +92,47 @@ def successor_processing(state, old_node, left_runners, right_runners):
                 # move 2 runners at a time across the bridge
                 new_state = State(state.runners_left - 2, 'Right', state.runners_right + 2,
                                   max(left_runners[0].run_time, left_runners[1].run_time))
-                if new_state.is_valid_cross():
-                    new_node = Node()
-                    new_node.state = new_state
-                    new_node.parent = old_node
-                    new_node.depth = old_node.depth + 1
-                    old_node.children.append(new_node)
-                    frontier.append(new_node)
-                    right_runners.append(left_runners[1])
-                    right_runners.append(left_runners[0])
-                    left_runners.remove(left_runners[1])
-                    left_runners.remove(left_runners[0])
 
-        if len(left_runners) == 1:
-            # maybe for some reason there is less than 2 people and need to bring only one to right
-            new_state = State(state.runners_left - 1, 'Right', state.runners_right + 1, left_runners[0].run_time)
-            if new_state.is_valid_cross():
-                new_node = Node
-                new_node.state = new_state
-                new_node.parent = old_node
-                new_node.depth = old_node.depth + 1
-                old_node.children.append(new_node)
-                frontier.append(new_node)
-                right_runners.append(left_runners[0])
-                left_runners.remove(left_runners[0])
-
-        return old_node, left_runners, right_runners
-
-    elif state.torch == 'Right':
-        for runner in right_runners:
-            new_state = State(state.runners_left + 1, 'Left', state.runners_right - 1, runner.run_time)
-            if new_state.is_valid_cross():
+                # set attributes of new node and move runners accordingly
                 new_node = Node()
                 new_node.state = new_state
                 new_node.parent = old_node
                 new_node.depth = old_node.depth + 1
                 old_node.children.append(new_node)
-                frontier.append(new_node)
-                left_runners.append(right_runners[0])
-                right_runners.remove(right_runners[0])
+                right_runners.append(left_runners[1])
+                right_runners.append(left_runners[0])
+                left_runners.remove(left_runners[1])
+                left_runners.remove(left_runners[0])
+
+        if len(left_runners) == 1:
+            # maybe for some reason there is less than 2 people and need to bring only one to right
+            new_state = State(state.runners_left - 1, 'Right',
+                              state.runners_right + 1, left_runners[0].run_time)
+
+            # set attributes of new node and move runners accordingly
+            new_node = Node
+            new_node.state = new_state
+            new_node.parent = old_node
+            new_node.depth = old_node.depth + 1
+            old_node.children.append(new_node)
+            right_runners.append(left_runners[0])
+            left_runners.remove(left_runners[0])
+
+        return old_node, left_runners, right_runners
+
+    elif state.torch == 'Right':
+        for runner in right_runners:
+            new_state = State(state.runners_left + 1, 'Left',
+                              state.runners_right - 1, runner.run_time)
+
+            # set attributes of new node and move runners accordingly
+            new_node = Node()
+            new_node.state = new_state
+            new_node.parent = old_node
+            new_node.depth = old_node.depth + 1
+            old_node.children.append(new_node)
+            left_runners.append(right_runners[0])
+            right_runners.remove(right_runners[0])
 
         return old_node, left_runners, right_runners
 
@@ -151,39 +142,43 @@ def successor_processing(state, old_node, left_runners, right_runners):
 
 def breadth_first():
     # initialize the root state and create a new root node with that state
-    left_side, torch, right_side, node, run_time = initiate_runners()
+    left_side, torch, right_side, node, run_time, total_runners = initiate_runners()
 
     path_to_goal = list()
     path_to_goal.append(node)
 
     while not node.state.is_at_goal():
+        # initialize the root state and create a new root node with that state
         node, left_side, right_side = successor_processing(node.state,
                                                            node,
                                                            left_side,
                                                            right_side)
+        # save the slowest run_time across bridge
         cheapest_run_time = 0
-        cheapest_node = Node()
+        curr_node = Node()
         for each_node in node.children:
             if not path_exists(path_to_goal, each_node):
                 if cheapest_run_time == 0 \
                    or each_node.state.run_time < cheapest_run_time:
                     cheapest_run_time = each_node.state.run_time
-                    cheapest_node = each_node
-        path_to_goal.append(cheapest_node)
-        run_time += cheapest_node.state.run_time
-        node = cheapest_node
+                    curr_node = each_node  # current node now has cheapest run time at it's depth
 
+        # append the node with the cheapest crossing time to the final path to the goal state
+        path_to_goal.append(curr_node)
+        run_time += curr_node.state.run_time
+        node = curr_node  # get successors / fringe from the cheapest node at this state
+
+    print '\nBreadth-first search chosen'
     return path_to_goal, run_time
 
 
 def depth_first():
     # initialize the root state and create a new root node with that state
-    left_side, torch, right_side, node, run_time = initiate_runners()
+    left_side, torch, right_side, node, run_time, total_runners = initiate_runners()
 
     path_to_goal = list()
     path_to_goal.append(node)
-
-    stack = list()
+    stack = list()  # use a stack for dfs
     already_processed = False
 
     while not node.state.is_at_goal():
@@ -193,24 +188,16 @@ def depth_first():
                                                                left_side,
                                                                right_side)
             already_processed = True
+            stack.extend(node.children)  # add the children to the top of the stack
 
-            for each_node in node.children:
-                stack.append(each_node)
-
-        new_node = Node()
-        new_node.state = stack[-1].state
-        new_node.parent = stack[-1].parent
-        new_node.depth = stack[-1].depth
-
-        stack.remove(stack[-1])
-
-        if not path_exists(path_to_goal, new_node) \
-           and new_node.state.is_valid_cross():
+        curr_node = stack.pop()  # take the latest child addition as current node
+        if not path_exists(path_to_goal, curr_node):
             already_processed = False
-            path_to_goal.append(new_node)
-            run_time += new_node.state.run_time
-            node = new_node
+            path_to_goal.append(curr_node)
+            run_time += curr_node.state.run_time
+            node = curr_node  # use this current node to get it's successors / fringe
 
+    print '\nDepth-first search chosen'
     return path_to_goal, run_time
 
 
@@ -218,6 +205,7 @@ def print_path():
     # path, run_time = breadth_first()
     path, run_time = depth_first()
     i = 0
+    print '\n'
     for node in path:
         print 'Move: ' + str(i)
         print str(node.state.runners_left) + ',  ' + node.state.torch + ',  ' + str(node.state.runners_right)
@@ -228,9 +216,11 @@ def print_path():
     print 'Total Time Elapsed: ' + str(run_time)
 
 
+# to be called at top level
 def main():
     print_path()
 
 
+# top level code
 if __name__ == '__main__':
     main()
