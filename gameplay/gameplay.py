a6c11248271711c7a1cb5106454b16258d124f41
Nick <proulx_nick06@hotmail.com>
2017-03-06 10:57:14 -0500

    added functionality to implement json configuration for indexing parameters

--- gameplay/gameplay.py
+++ gameplay/gameplay.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 
 class GamePiece:
@@ -16,6 +17,7 @@ class Node:
         self.depth = 0
         self.children = list()  # to be used to create possible child nodes dynamically and append to list
         self.heuristic_value = 0
+        self.pieces_captured = 0
 
 
 def initiate_board():
@@ -60,8 +62,31 @@ def copy_list(old_list):
     return new_list
 
 
-def successor_processing(board):
-    pass
+# 'player' is a string denoting the color of the current player making a move at this instance
+def successor_processing(board, player):
+    json_path = 'C:\Users\Nick\PycharmProjects\ArtificialIntelligence\index_config\index_config.json'
+    with open(json_path) as data_file:
+        indexing_params = json.load(data_file)
+    x_sums = [4, 10, 18, 26, 34, 42, 48]
+    y_sums = [1, 5, 11, 19, 27, 35, 43, 49]
+
+    i = 0  # indexing value
+    j = 1  # corresponding x-axis value
+    k = 1  # corresponding y-axis value
+    for piece in board:
+        if piece.color == 'empty':
+            continue
+        elif piece.color == 'green':
+            stack = piece.height  # for control - need to make multiple moves with stacks while splitting the stack
+            while stack > 0:
+                # move up for all possible stack sizes
+                if stack == 5 and k == 6 \
+                   and 7 > j > 2 \
+                   or stack == 5 and k == 7 \
+                   or stack == 5 and k == 8:
+                    game_piece = GamePiece()
+                    game_piece.color = 'green'
+                    game_piece.height = stack
 
 
 def print_board(board):
@@ -76,6 +101,10 @@ def print_board(board):
 
 # to be called at top level
 def main():
+    json_path = 'C:\Users\Nick\PycharmProjects\ArtificialIntelligence\index_config\index_config.json'
+    with open(json_path) as data_file:
+        indexing_params = json.load(data_file)
+    x = str(indexing_params["indexes"]["1"]["hi"])
     board = initiate_board()
     print_board(board)
 
