3c5e60285a263e13b45d0f219b0beda97bf1ebfd
Nick <proulx_nick06@hotmail.com>
2017-03-28 20:12:11 -0400

    initial commit

---
+++ classification/classification.py
@@ -0,0 +1,20 @@
+import random
+
+
+class Tree:
+
+    def __init__(self):
+        self.nodes = list()
+
+
+class Node:
+
+    def __init__(self):
+        self.state = list()
+        self.parent = None  # which node this depends on
+        self.vector_data = list()
+        self.depth = 0
+
+
+def generate_class_tree():
+    pass
