f11b0044df8a034cd1ae8315e7f532e1a0cad003
Nick <proulx_nick06@hotmail.com>
2017-03-29 17:27:47 -0400

    generating random vector data properly for each class

--- classification/classification.py
+++ classification/classification.py
@@ -12,6 +12,7 @@ class Trait:  # the nodes that make up the tree
     def __init__(self):
         self.value = 0  # the trait number (1-10)
         self.parent = None  # which trait this node depends on
+        self.parent_index = None
         self.depth = 0
         self.probability = 0.0
         self.parent_probability = 0.0
@@ -31,8 +32,10 @@ def generate_class_tree():
         tree.traits.append(trait)
         if 7 >= i > 1:
             trait.parent = values[i - 2]
+            trait.parent_index = i - 2
         elif i > 0:
             trait.parent = values[i - 1]
+            trait.parent_index = i - 1
 
         if i == 0 or i % 2 == 0 or i >= 7:
             depth += 1
@@ -52,13 +55,43 @@ def generate_probabilities(tree):
 
 
 def generate_vector_data(tree, probability_dict):
-    pass
+    vector_list = list()
+
+    i = 1
+    while i <= 2000:
+        data_list = list()
+        j = 0
+        for trait in tree.traits:
+            if j == 0:
+                data_list.append(int(random.choice('01')))
+            else:
+                parent_index = trait.parent_index
+                parent_probability = probability_dict[trait.parent]
+                random_value = round(random.random(), 2)
+
+                if data_list[parent_index] == 0:
+                    if random_value <= parent_probability:
+                        data_list.append(0)
+                    else:
+                        data_list.append(1)
+
+                elif data_list[parent_index] == 1:
+                    if random_value >= parent_probability:
+                        data_list.append(0)
+                    else:
+                        data_list.append(1)
+
+            j += 1
+        i += 1
+        vector_list.append(data_list)
+    return vector_list
 
 
 # to be called at top level
 def main():
     tree = generate_class_tree()
     tree, probabilities = generate_probabilities(tree)
+    vector_data = generate_vector_data(tree, probabilities)
     for trait in tree.traits:
         print str(trait.value) + ' -- ' + str(trait.depth) + ' -- ' + str(trait.parent)
 
