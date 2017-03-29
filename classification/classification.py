8b0b90006bc798afbc63199b90ccf62ec0de3c6a
Nick <proulx_nick06@hotmail.com>
2017-03-29 15:22:32 -0400

    properly generating class tree

--- classification/classification.py
+++ classification/classification.py
@@ -4,17 +4,48 @@ import random
 class Tree:
 
     def __init__(self):
-        self.nodes = list()
+        self.traits = list()
 
 
-class Node:
+class Trait:  # the nodes that make up the tree
 
     def __init__(self):
-        self.state = list()
-        self.parent = None  # which node this depends on
-        self.vector_data = list()
+        self.value = 0  # the trait number (1-10)
+        self.parent = None  # which trait this node depends on
         self.depth = 0
+        self.probability = 0.0
 
 
 def generate_class_tree():
-    pass
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    random.shuffle(values)
+    tree = Tree()
+
+    i = 0
+    depth = 0
+    while i < len(values):
+        trait = Trait()
+        trait.value = values[i]
+        trait.depth = depth
+        tree.traits.append(trait)
+        if 7 >= i > 1:
+            trait.parent = values[i - 2]
+        elif i > 0:
+            trait.parent = values[i - 1]
+
+        if i == 0 or i % 2 == 0 or i >= 7:
+            depth += 1
+        i += 1
+
+    return tree
+
+
+# to be called at top level
+def main():
+    tree = generate_class_tree()
+    for trait in tree.traits:
+        print str(trait.value) + ' -- ' + str(trait.depth) + ' -- ' + str(trait.parent)
+
+# top level code
+if __name__ == '__main__':
+    main()
