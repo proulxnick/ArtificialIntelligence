d54cd7044ff6c3969275d84c465468418aa6fdbc
Nick <proulx_nick06@hotmail.com>
2017-04-08 16:05:25 -0400

    starting to process the successors

--- space_management/rubiks_cube.py
+++ space_management/rubiks_cube.py
@@ -64,7 +64,36 @@ def process_moves(curr_node, cube_size):
     # this function calculates and returns all possible moves and returns
     # each as a new state in a newly constructed node
 
-    pass
+    cube_state = curr_node.state  # state passed in - this will be copied and manipulated
+    side_total = math.pow(cube_size, 2)
+
+    # move up front
+    row = 1
+    column = 1
+    i = side_total * 5  # first index at front of cube
+    j = side_total  # first index at top of cube
+    k = side_total * 4  # first index at back of cube
+    l = side_total * 3  # first index at bottom of cube
+    while row <= cube_size:
+        new_state = copy_list(cube_state)
+        while column <= cube_size:
+            new_state[j] = cube_state[i]  # front -> top
+            new_state[k] = cube_state[j]  # top -> back
+            new_state[l] = cube_state[k]  # back -> bottom
+            new_state[i] = cube_state[l]  # bottom -> front
+
+            i += cube_size
+            j += cube_size
+            k += cube_size
+            l += cube_size
+            column += 1
+        # create new node with new_state
+        new_node = Node()
+        new_node.state = new_state
+        new_node.parent = curr_node
+        new_node.depth = curr_node.depth + 1
+        curr_node.children.append(new_node)
+        row += 1
 
 
 # to be called at top level
