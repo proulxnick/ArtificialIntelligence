5158d6cd545bff9939c2c2f635987f75adb6b886
Nick <proulx_nick06@hotmail.com>
2017-04-08 14:43:40 -0400

    added function to shuffle initial state of the cube

--- space_management/rubiks_cube.py
+++ space_management/rubiks_cube.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 
 class BadStateException(Exception):
@@ -17,9 +18,50 @@ class Node:
         self.heuristic_value = 0
 
 
+def copy_list(old_list):
+    # used to copy contents of one list and create another list
+
+    new_list = list()
+    for i in old_list:
+        new_list.append(i)
+
+    return new_list
+
+
+def randomize_cube():
+    valid_size = False
+    initial_state = list()
+    size = 0
+
+    # get the user to input the degree of the cube
+    while not valid_size:
+        size = input("Enter the degree/size cube you wish to solve (e.g 3 = 3x3 Rubik's Cube)")
+        if size > 1:
+            valid_size = True
+        else:
+            print "Cube size must be greater than 1"
+
+    # now populate the list of tiles which will be the original state
+    count1 = 0
+    count2 = 0
+    i = 1
+    while count1 < 6:
+        while count2 < math.pow(size, 2):
+            initial_state.append(i)
+            count2 += 1
+        count1 += 1
+        count2 = 0
+        i += 1
+
+    random.shuffle(initial_state)  # randomize the order of the cube
+
+    return initial_state
+
+
 # to be called at top level
 def main():
-    pass
+    state = randomize_cube()
+    print state
 
 # top level code
 if __name__ == '__main__':
