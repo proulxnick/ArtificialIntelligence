5980f7eda4722d62806951c55c4cd3c7130e3116
Nick <proulx_nick06@hotmail.com>
2017-04-11 19:41:21 -0400

    initial commit

---
+++ index_config/shuffle_moves.py
@@ -0,0 +1,22 @@
+def move_1(state):
+    pass
+
+
+def move_2(state):
+    pass
+
+
+def move_3(state):
+    pass
+
+
+def move_4(state):
+    pass
+
+
+def move_5(state):
+    pass
+
+
+def move_6(state):
+    pass
