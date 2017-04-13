3a911c6f1beb21370d3629367fa014241bba83fe
Nick <proulx_nick06@hotmail.com>
2017-04-13 18:24:38 -0400

    updated tile object attributes

--- index_config/shuffle_moves.py
+++ index_config/shuffle_moves.py
@@ -50,9 +50,25 @@ def move_1(cube_state, cube_size):
     limit = 0
     while limit < cube_size:
         new_state[j] = cube_state[i]  # front -> top
+        new_state[j].current = j
+        new_state[j].column = column + 1
+        new_state[j].row = limit + 1
+
         new_state[k] = cube_state[j]  # top -> back
+        new_state[k].current = k
+        new_state[k].column = column + 1
+        new_state[k].row = limit + 1
+
         new_state[l] = cube_state[k]  # back -> bottom
+        new_state[l].current = l
+        new_state[l].column = column + 1
+        new_state[l].row = limit + 1
+
         new_state[i] = cube_state[l]  # bottom -> front
+        new_state[i].current = j
+        new_state[i].column = column + 1
+        new_state[i].row = limit + 1
+
         if column == 0 and not rotation:  # left side rotates
             rotation = True
             z = 0
@@ -107,10 +123,26 @@ def move_2(cube_state, cube_size):
     rotation = False
     limit = 0
     while limit < cube_size:
-        new_state[j] = cube_state[i]  # front -> top
-        new_state[k] = cube_state[j]  # top -> back
-        new_state[l] = cube_state[k]  # back -> bottom
-        new_state[i] = cube_state[l]  # bottom -> front
+        new_state[l] = cube_state[i]  # front -> bottom
+        new_state[l].current = l
+        new_state[l].column = column + 1
+        new_state[l].row = limit + 1
+
+        new_state[k] = cube_state[l]  # bottom -> back
+        new_state[k].current = l
+        new_state[k].column = column + 1
+        new_state[k].row = limit + 1
+
+        new_state[j] = cube_state[k]  # back -> top
+        new_state[j].current = j
+        new_state[j].column = column + 1
+        new_state[j].row = limit + 1
+
+        new_state[i] = cube_state[j]  # top -> front
+        new_state[i].current = i
+        new_state[i].column = column + 1
+        new_state[i].row = limit + 1
+
         if column == 0 and not rotation:  # left side rotates
             rotation = True
             z = 0
@@ -163,13 +195,28 @@ def move_3(cube_state, cube_size):
     n = side_total * 3  # first index at bottom of cube
     limit = 0
     new_state = copy_list(cube_state)
-    column = 0
     rotation = False
     while limit < cube_size:
         new_state[j] = cube_state[i]  # front -> right
+        new_state[j].current = j
+        new_state[j].column = limit + 1
+        new_state[j].row = column + 1
+
         new_state[k] = cube_state[j]  # right -> back
+        new_state[k].current = k
+        new_state[k].column = limit + 1
+        new_state[k].row = column + 1
+
         new_state[l] = cube_state[k]  # back -> left
+        new_state[l].current = l
+        new_state[l].column = limit + 1
+        new_state[l].row = column + 1
+
         new_state[i] = cube_state[l]  # left -> front
+        new_state[i].current = i
+        new_state[i].column = limit + 1
+        new_state[i].row = column + 1
+
         if column == 0 and not rotation:  # top side rotates
             rotation = True
             z = 0
@@ -222,13 +269,28 @@ def move_4(cube_state, cube_size):
     n = side_total * 3  # first index at bottom of cube
     limit = 0
     new_state = copy_list(cube_state)
-    column = 0
     rotation = False
     while limit < cube_size:
-        new_state[j] = cube_state[i]  # front -> right
-        new_state[k] = cube_state[j]  # right -> back
-        new_state[l] = cube_state[k]  # back -> left
-        new_state[i] = cube_state[l]  # left -> front
+        new_state[l] = cube_state[i]  # front -> left
+        new_state[l].current = l
+        new_state[l].column = limit + 1
+        new_state[l].row = column + 1
+
+        new_state[k] = cube_state[l]  # left -> back
+        new_state[k].current = k
+        new_state[k].column = limit + 1
+        new_state[k].row = column + 1
+
+        new_state[j] = cube_state[k]  # back -> right
+        new_state[j].current = j
+        new_state[j].column = limit + 1
+        new_state[j].row = column + 1
+
+        new_state[i] = cube_state[j]  # right -> front
+        new_state[i].current = i
+        new_state[i].column = limit + 1
+        new_state[i].row = column + 1
+
         if column == 0 and not rotation:  # top side rotates
             rotation = True
             z = 0
@@ -284,9 +346,25 @@ def move_5(cube_state, cube_size):
     rotation = False
     while limit < cube_size:
         new_state[j] = cube_state[i]  # left -> top
+        new_state[j].current = j
+        new_state[j].column = column + 1
+        new_state[j].row = limit + 1
+
         new_state[k] = cube_state[j]  # top -> right
+        new_state[k].current = k
+        new_state[k].column = column + 1
+        new_state[k].row = limit + 1
+
         new_state[l] = cube_state[k]  # right -> bottom
+        new_state[l].current = l
+        new_state[l].column = column + 1
+        new_state[l].row = limit + 1
+
         new_state[i] = cube_state[l]  # bottom -> left
+        new_state[i].current = i
+        new_state[i].column = column + 1
+        new_state[i].row = limit + 1
+
         if column == 0 and not rotation:  # left side rotates
             rotation = True
             z = 0
@@ -341,10 +419,26 @@ def move_6(cube_state, cube_size):
     limit = 0
     rotation = False
     while limit < cube_size:
-        new_state[j] = cube_state[i]  # left -> top
-        new_state[k] = cube_state[j]  # top -> right
-        new_state[l] = cube_state[k]  # right -> bottom
-        new_state[i] = cube_state[l]  # bottom -> left
+        new_state[l] = cube_state[i]  # left -> bottom
+        new_state[l].current = l
+        new_state[l].column = column + 1
+        new_state[l].row = limit + 1
+
+        new_state[k] = cube_state[l]  # bottom -> right
+        new_state[k].current = k
+        new_state[k].column = column + 1
+        new_state[k].row = limit + 1
+
+        new_state[j] = cube_state[k]  # right -> top
+        new_state[j].current = j
+        new_state[j].column = column + 1
+        new_state[j].row = limit + 1
+
+        new_state[i] = cube_state[j]  # top -> left
+        new_state[i].current = i
+        new_state[i].column = column + 1
+        new_state[i].row = limit + 1
+
         if column == 0 and not rotation:  # left side rotates
             rotation = True
             z = 0
